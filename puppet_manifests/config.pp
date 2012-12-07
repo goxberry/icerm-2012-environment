917b9d485d1d55d1e863377ade15d0a6e9c1b3bb
Geoffrey Malcolm Oxberry <goxberry@gmail.com>
2012-12-07 01:57:26 -0800

    Specify environment for SciPy development.
    Specify environment for SciPy development by adding the following
    material:
    
    puppet_manifests: Puppet files (manifests) that specify configuration
    puppet_manifests/config.pp: specifies configuration for VM
    puppet_modules: contains modules that add functionality to Puppet
    puppet_modules/stdlib: Standard library for Puppet modules
    puppet_modules/apt: Adds advanced functionality for Apt package manager
    puppet_modules/vcsrepo: Adds ability to pull in VCS repos

---
+++ puppet_manifests/config.pp
@@ -0,0 +1,86 @@
+# Puppet manifest to provision a virtual machine for "Turnkey Reproducibility"
+# presentation at ICERM 2012: Reproducibility in Experimental and
+# Computational Mathematics.
+# IMPORTANT NOTE: Puppet is a DECLARATIVE domain-specific language,
+# so the order of the declaration of resources (file, apt::source, etc.)
+# is immaterial; the order of execution only respects explicit
+# dependencies (using the require, before, after, ->, <-, etc.)
+# directives. See documentation for Puppet and the book "Pro Puppet"
+# for full details of the Puppet language.
+
+# TODO(goxberry@gmail.com): Write a script that pulls this setup from a
+# Git repo of mine, then runs "vagrant up" and "vagrant provision".
+# Make it a bash script, and tell Windows users to run it from the
+# Git Bash shell.
+
+group { "puppet":
+        ensure => "present",
+    }
+
+# This file needed for package repository updates.
+file { '/etc/apt/sources.list':
+  ensure => present,
+  }
+
+# This file needed to modify sources list via apt::ppa and apt::source
+file { '/etc/apt/sources.list.d':
+  ensure => present,
+  }
+
+# Include the Precise Pangolin Ubuntu 12.04 LTS sources list,
+# and make it a high priority so that it is the default
+# sources list.
+apt::source { 'precise':
+  location          => "http://us.archive.ubuntu.com/ubuntu/",
+  release           => "precise",
+  repos             => "main restricted universe multiverse",
+  pin               => "900",
+  include_src       => true,
+  require           => [ File['/etc/apt/sources.list.d'],
+                         File['/etc/apt/sources.list'] ],
+}
+
+# Ensure that virtual machine gets the latest operating system
+# updates first.
+# Taken from:
+# http://stackoverflow.com/questions/10845864/puppet-trick-run-apt-get-update-before-installing-other-packages
+exec { 'apt-get update':
+  command => '/usr/bin/apt-get update',
+  require => [ File['/etc/apt/sources.list'],
+               File['/etc/apt/sources.list.d'] ],
+  }
+
+# Install basic packages needed for a machine with a graphical interface.
+# Inspired by http://escience.washington.edu/get-help-now/create-virtual-machine-software-bundling-or-reproducible-research
+# Added BLAS and LAPACK packages for scipy, pip to pull in Python packages.
+# Remaining packages are personal preference.
+apt::force { ['xorg', 'xfce4', 'xfce4-terminal', 'xfce4-clipman', 'firefox',
+ 'gdm', 'build-essential', 'g++', 'python', 'scons', 'libboost-all-dev',
+ 'subversion', 'python-dev', 'ipython',
+ 'ipython-notebook', 'ipython-qtconsole', 'git', 'emacs', 'git-svn',
+  'gfortran', 'python-pip', 'libblas-dev',  'liblapack-dev',
+  'puppet', 'lynx', 'cython', 'python-nose', 'python-pudb' ] :
+  release => 'precise',
+  require => Apt::Source['precise']
+  }
+
+# Install latest stable NumPy
+package {'numpy':
+  ensure => latest,
+  provider => pip,
+  require => Apt::Force['python-pip']
+  }
+
+# Install latest stable SciPy
+package {'scipy':
+  ensure => latest,
+  provider => pip,
+  require => [ Apt::Force['python-pip'],
+               Apt::Force['build-essential'],
+               Apt::Force['gfortran'],
+               Apt::Force['libblas-dev'],
+               Apt::Force['liblapack-dev'],
+               Apt::Force['python-nose'],
+               Apt::Force['cython'],
+               Package['numpy'] ]
+  }
